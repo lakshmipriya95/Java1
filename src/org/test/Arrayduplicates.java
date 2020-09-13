5f95fbbe2b99f96ecfb9f6a36cae9e0809301adb
lakshmi95 <lakshmi2010green@gmail.com>
2020-09-12 17:02:49 -0700

    Upload in master branch

---
+++ src/org/test/Arrayduplicates.java
@@ -0,0 +1,19 @@
+package org.test;
+	public class Arrayduplicates{
+		 
+	    public void main(String[] args) {
+	    	String[] thisIsAStringArray = {"AAA", "BBB", "CCC", "DDD", "EEE","FFF","GGG","HHH","III","JJJ"};
+	    	
+	    	System.out.println( thisIsAStringArray[0] );
+	    	System.out.println( thisIsAStringArray[1] );
+	    	System.out.println( thisIsAStringArray[2] );
+	    	System.out.println( thisIsAStringArray[3] );
+	    	System.out.println( thisIsAStringArray[4] );
+	    	for( String element: thisIsAStringArray)
+	    	{
+	    	    
+	    	    System.out.println( element );    
+	    	}
+	    	
+	    }
+	}
